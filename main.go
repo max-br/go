059611aa9f238f446218952fa0dcadd3c718229f
max-br <max.breitenfeldt@gmail.com>
2015-09-06 11:56:42 +0200

    REFACTOR: pass board struct as a method receiver, dont save movelist in board struct

--- main.go
+++ main.go
@@ -5,36 +5,35 @@ import (
 )
 
 const (
-	boardstring = "............ooooobbbb..oooooobbb..ooooooobb..oooooooob..ooooooooo..woooooooo..wwooooooo..wwwoooooo..wwwwooooo............"
-	debugstring = "............ooooobbbb..oooooobob..ooooooobb..ooooooooo..ooooobobo..ooowboboo..ooooooooo..ooooooooo..ooooooooo............"
+	boardstring = "############.....BBBB##......BBB##.......BB##........B##.........##W........##WW.......##WWW......##WWWW.....############"
+	debugstring = "############.....BBBB##......B.B##.......BB##.........##.....B.B.##...wB.B..##.........##.........##.........############"
 	NORT        = -11
 	SOUT        = 11
 	WEST        = -1
 	EAST        = 1
-	EMPTY       = 'o'
-	OFFBD       = '.'
-	BLACK       = 'b'
-	WHITE       = 'w'
+	EMPTY       = '.'
+	OFFBD       = '#'
+	BLACK       = 'B'
+	WHITE       = 'W'
 )
 
 var directions = [...]int{NORT, SOUT, WEST, EAST}
-var board Board
 
 type Move struct {
 	from, to int
 }
 
 type Board struct {
-	field      [121]byte
-	moves      map[Move]bool
+	field [121]byte
+	//moves      map[Move]bool
 	moverecord [128]Move
 	recordcnt  int
 	us         byte
 	them       byte
 }
 
-func InitBoard() {
-	board.moves = make(map[Move]bool)
+func (board *Board) InitBoard() {
+	//board.moves = make(map[Move]bool)
 	for i := 0; i != len(boardstring); i++ {
 		board.field[i] = boardstring[i]
 	}
@@ -44,28 +43,30 @@ func InitBoard() {
 	return
 }
 
-func ToString() (out string) {
+func (board *Board) ToString() (out string) {
 	for i := 0; i != len(boardstring); i++ {
 		if i%11 == 0 && i != 0 {
 			out += string('\n')
 		}
 		out += string(board.field[i])
+		out += string(' ')
 	}
+	out += string('\n')
 	return
 }
 
 /* Returns all legal steps for a figure */
-func GenerateSteps(from int) {
+func (board *Board) GenerateSteps(from int, moves map[Move]bool) {
 	for _, dir := range directions {
 		to := from + dir
 		if board.field[to] == EMPTY {
 			move := Move{from, to}
-			board.moves[move] = true
+			moves[move] = true
 		}
 	}
 }
 
-func Jumps(from int) (jumps []int) {
+func (board *Board) Jumps(from int) (jumps []int) {
 	for _, dir := range directions {
 		onestep := from + dir
 		if board.field[onestep] == BLACK || board.field[onestep] == WHITE {
@@ -78,26 +79,26 @@ func Jumps(from int) (jumps []int) {
 	return
 }
 
-func GenerateJumps(from int, curr int) {
-	for _, to := range Jumps(curr) {
+func (board *Board) GenerateJumps(from int, curr int, moves map[Move]bool) {
+	for _, to := range board.Jumps(curr) {
 		move := Move{from, to}
-		if !board.moves[move] {
-			board.moves[move] = true
-			GenerateJumps(from, to)
+		if !moves[move] {
+			moves[move] = true
+			board.GenerateJumps(from, to, moves)
 		}
 	}
 }
 
-func GenerateMoves() {
+func (board *Board) GenerateMoves(moves map[Move]bool) {
 	for i, f := range board.field {
 		if f == board.us {
-			GenerateSteps(i)
-			GenerateJumps(i, i)
+			board.GenerateJumps(i, i, moves)
+			board.GenerateSteps(i, moves)
 		}
 	}
 }
 
-func MakeMove(move Move) {
+func (board *Board) MakeMove(move Move) {
 	board.field[move.to] = board.field[move.from]
 	board.field[move.from] = EMPTY
 	board.moverecord[board.recordcnt] = move
@@ -105,7 +106,7 @@ func MakeMove(move Move) {
 	board.us, board.them = board.them, board.us
 }
 
-func UnMakeMove() {
+func (board *Board) UnMakeMove() {
 	board.recordcnt--
 	move := board.moverecord[board.recordcnt]
 	board.field[move.from] = board.field[move.to]
@@ -127,26 +128,30 @@ func IndexToCoord(index int) (x int, y int) {
 	return
 }
 
-func DistanceToGoal(sq int) (distance int) {
-	x, y := IndexToCoord(sq)
-	goalX, goalY := 0, 0
-	if board.us == WHITE {
+func Goal(color byte) (goalX int, goalY int) {
+	if color == WHITE {
 		goalX, goalY = 9, 1
 	} else {
 		goalX, goalY = 1, 9
 	}
-	distX, distY := Abs(x-goalX), Abs(y-goalY)
-	distance = Abs(distX + distY)
 	return
 }
 
-func Evaluate() (score int) {
-	for sq, f := range board.field {
+func DistanceTo(index int, goalX int, goalY int) (distance int) {
+	x, y := IndexToCoord(index)
+	distance = Abs(x-goalX) + Abs(y-goalY)
+	return
+}
+
+func (board *Board) Evaluate() (score int) {
+	for index, f := range board.field {
 		if f == board.us {
-			score -= DistanceToGoal(sq)
+			goalX, goalY := Goal(board.us)
+			score -= DistanceTo(index, goalX, goalY)
 		}
 		if f == board.them {
-			score += DistanceToGoal(sq)
+			goalX, goalY := Goal(board.them)
+			score += DistanceTo(index, goalX, goalY)
 		}
 	}
 	return
@@ -154,59 +159,75 @@ func Evaluate() (score int) {
 
 var cnt int = 0
 
-func AlphaBeta(depth int, alpha int, beta int, bestmove *Move) int {
+func (board *Board) AlphaBeta(depth int, alpha int, beta int) int {
 	cnt++
 	if depth == 0 {
-		return Evaluate()
+		return board.Evaluate()
 	}
-
-	board.moves = make(map[Move]bool)
-	GenerateMoves()
+	moves := make(map[Move]bool)
+	board.GenerateMoves(moves)
 	val := 0
 
-	for move, _ := range board.moves {
-		MakeMove(move)
-		val = -AlphaBeta(depth-1, -beta, -alpha, bestmove)
-		UnMakeMove()
+	for move, _ := range moves {
+		board.MakeMove(move)
+		val = board.AlphaBeta(depth-1, -beta, -alpha)
+		board.UnMakeMove()
 		if val >= beta {
 			return beta
 		}
 		if val > alpha {
 			alpha = val
-			bestmove = &move
 		}
 	}
 	return alpha
 }
 
-func Perft(depth int) (nodes int) {
+func (board *Board) SearchBestMove(depth int) (bestmove Move) {
+	moves := make(map[Move]bool)
+	board.GenerateMoves(moves)
+	bestscore := -50000
+	for move, _ := range moves {
+		board.MakeMove(move)
+		score := -board.AlphaBeta(depth-1, -10000, 10000)
+		if score > bestscore {
+			bestmove = move
+			bestscore = score
+			fmt.Println(move, score)
+		}
+		board.UnMakeMove()
+	}
+	return
+}
+
+func (board *Board) Perft(depth int) (nodes int) {
 	if depth == 0 {
 		return 1
 	}
-	board.moves = make(map[Move]bool)
-	GenerateMoves()
-	for move, _ := range board.moves {
-		MakeMove(move)
-		nodes += Perft(depth - 1)
-		UnMakeMove()
+	moves := make(map[Move]bool)
+	board.GenerateMoves(moves)
+	for move, _ := range moves {
+		board.MakeMove(move)
+		nodes += board.Perft(depth - 1)
+		board.UnMakeMove()
 	}
 	return nodes
 }
 
-func Divide(depth int) {
-	board.moves = make(map[Move]bool)
-	GenerateMoves()
-	fmt.Println(len(board.moves))
-	for move, _ := range board.moves {
-		MakeMove(move)
+func (board *Board) Divide(depth int) {
+	moves := make(map[Move]bool)
+	board.GenerateMoves(moves)
+	fmt.Println(len(moves))
+	for move, _ := range moves {
+		board.MakeMove(move)
 		x, y := IndexToCoord(move.from)
 		x2, y2 := IndexToCoord(move.to)
-		fmt.Println(Perft(depth-1), move, "from: ", x, y, "to: ", x2, y2)
-		UnMakeMove()
+		fmt.Println(board.Perft(depth-1), move, "from: ", x, y, "to: ", x2, y2)
+		board.UnMakeMove()
 	}
 }
 
 func main() {
-	InitBoard()
-	fmt.Println(Perft(2))
+	var board Board
+	board.InitBoard()
+	fmt.Println(board.Perft(5)) // should be 1381888
 }
